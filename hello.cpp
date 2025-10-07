4df859398575ee4184b2564574e68e267a23fd28
Harsh Guleria <guleriah59@gmail.com>
2025-10-07 11:00:04 +0530

    Hello chai

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,37 @@
+// Chai aur C++ ->
+
+#include <iostream>
+
+using namespace std;
+
+// int main(){
+     // cout << "Hello Chai From : Harsh";
+// }
+
+// Going in depth of Hello World ->
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+// int main(){
+//     cout << "Hello Chai from Harry" << endl;
+// }
+
+#include <iostream>
+
+// int main(){
+//     std::cout << "Hello Chai from Gullu" << std::endl;
+// }
+
+// Return 0 ->
+
 #include <iostream>
 
 using namespace std;
 
 int main(){
-    cout << "Hello Chai From : Harsh";
+    cout<< "Hello Chai From Harsh" << endl;
+    return 0;
 }
