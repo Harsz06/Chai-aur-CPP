c3a979afa8bfacc021ed9057d999a193cf19a21d
Harsh Guleria <guleriah59@gmail.com>
2025-10-09 10:59:37 +0530

    05_function

---
+++ 06_function/myFunction.cpp
@@ -0,0 +1,40 @@
+// returnType functionName(parameters){
+//     // function body
+// }
+// void -> empty in C++
+
+#include <iostream>
+using namespace std;
+
+//declaration of function
+void serveChai(int cups);
+
+void makeChai(){
+    cout << "Boiling water, adding tea leaves, straining..." << '\n';
+}
+
+int checkTemperture(int temperature){
+    return temperature;
+}
+
+void serveChai(string teaType = "Masala Tea"){
+    int cups = 4;
+    cout << "Serving " << teaType << endl;
+}
+
+int main(){
+    int temp = checkTemperture(50);
+    // std::cout << temp << '\n' << std::endl;
+
+    // makeChai(); //calling a function
+
+    // serveChai(3);
+
+    serveChai();
+    return 0;
+}
+
+//defination of function
+void serveChai(int cups){
+    cout << "Serving " << cups << " of chai" << endl;
+}
