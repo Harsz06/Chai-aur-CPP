487c349dcaeb389dcdf544b1500c3f8edd4d96fe
Harsh Guleria <guleriah59@gmail.com>
2025-10-10 21:45:09 +0530

    08_array

---
+++ 08_array/two.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int chaiServed[7] = {85, 90, 88, 92, 89, 75, 60};
+
+    cout << "Chai cups served on Day 1: " << chaiServed[0] << endl;
+    cout << "Chai cups served on Day 2: " << chaiServed[1] << endl;
+
+
+    return 0;
+}
