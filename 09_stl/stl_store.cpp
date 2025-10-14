89f5ad25d7acfc5b64d7234059389305cc2f5a6d
Harsh Guleria <guleriah59@gmail.com>
2025-10-14 12:10:25 +0530

    09_stl

--- 09_stl/stl_store.cpp
+++ 09_stl/stl_store.cpp
@@ -26,11 +26,33 @@ struct Order{
 };
 
 int main(){
-    vector<Product> prodeucts = {
+    vector<Product> products = {
         {101, "Laptop", "Electronics"},
         {102, "SmartPhone", "Electronics"},
         {103, "Coffee Maker", "Kitchen"},
         {104, "Blender", "Kitchen"},
         {105, "Desk Lamp", "Home"},
     };
+
+    deque<string> recentCustomers = {"C001", "C002", "C003"};
+
+    recentCustomers.push_back("C004");
+    recentCustomers.push_front("C005");
+
+    list<Order> orderHistroy;
+
+    orderHistroy.push_back({1, 101, 1, "C001", time(0)});
+    orderHistroy.push_back({2, 102, 2, "C002", time(0)});
+    orderHistroy.push_back({3, 103, 1, "C003", time(0)});
+
+    set<string> categories;
+
+    for(const auto &product: products){
+        categories.insert(product.category);
+    }
+
+    
+
+
+    return 0;
 }
