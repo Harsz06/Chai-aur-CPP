78624b788bbc9868501d0727c1a4f0b97448ce95
Harsh Guleria <guleriah59@gmail.com>
2025-10-14 22:22:36 +0530

    09_stl

--- 09_stl/stl_emp_manage.cpp
+++ 09_stl/stl_emp_manage.cpp
@@ -13,7 +13,7 @@ struct Employee {
     double salary;
 };
 
-void displayEMployee(const Employee& emp){
+void displayEmployee(const Employee& emp){
     cout << "ID: " << emp.id << "Name: " << emp.name << " , Salary: $" << emp.salary << endl;
 }
 
@@ -27,7 +27,35 @@ int main(){
         {101, "aryan", 70000},
     };
 
-    
+    sort(employees.begin(), employees.end(), [](const Employee& e1, Employee& e2){
+        return e1.salary > e2.salary;
+    });
+
+    cout << "Employees sorted by salary -> Highest to lowest \n";
+    for_each(employees.begin(),employees.end(),displayEmployee);
+
+    vector <Employee> highEarners;
+
+    copy_if(
+        employees.begin(),
+        employees.end(),
+        back_inserter(highEarners),
+        [](const Employee& e){
+        return e.salary > 50000;
+    });
+
+    cout << "Employees who are high earners \n";
+    for_each(highEarners.begin(),highEarners.end(),displayEmployee);
+
+    double totalSalary = accumulate(employees.begin(), employees.end(), 0.0, [](double sum, const Employee& e){
+        return sum + e.salary;
+    });
+
+    double averageSalary = totalSalary / employees.size();
+
+    auto highestPaid = max_element(employees.begin(), employees.end(), [](const Employee& e1, Employee& e2){
+        return e1.salary < e2.salary;
+    });
 
     return 0;
 }
